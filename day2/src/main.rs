495379f00311e1fdda390b543e0ff6d311759022
Risto "Dilaz" Viitanen <risto.viitanen@iki.fi>
2023-12-03 11:03:39 +0200

    Add day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -11,10 +11,14 @@ const GREEN_STR: &str = "green";
 const BLUE_STR: &str = "blue";
 
 fn main() {
-    part1();
+    // Part 1
+    parts(false);
+
+    // Part 2
+    parts(true);
 }
 
-fn part1() {
+fn parts(minimum_values: bool) {
     let content = fs::read_to_string(FILE_PATH).expect("Failed to read file");
 
     let sum: u32 = content
@@ -23,6 +27,7 @@ fn part1() {
         let mut iter = line.split(": ");
         let game_name = iter.next().unwrap();
         let rounds = iter.next().unwrap().split("; ");
+        let mut game_cube_colors = HashMap::<&str, u32>::new();
         for round in rounds {
             let mut cube_colors = HashMap::<&str, u32>::new();
             let cubes = round.split(", ");
@@ -31,19 +36,37 @@ fn part1() {
                 let num = iter.next().unwrap().parse::<u32>().unwrap();
                 let color = iter.next();
 
-                match color.unwrap() {
-                    RED_STR => cube_colors.entry(RED_STR).and_modify(|n| *n += num).or_insert(num),
-                    GREEN_STR => cube_colors.entry(GREEN_STR).and_modify(|n| *n += num).or_insert(num),
-                    BLUE_STR => cube_colors.entry(BLUE_STR).and_modify(|n| *n += num).or_insert(num),
-                    _ => panic!("Invalid color")
-                };
+                if minimum_values {
+                    println!("{}: {} {}", game_name, num, color.unwrap());
+                    match color.unwrap() {
+                        RED_STR => game_cube_colors.entry(RED_STR).and_modify(|n| if *n < num { *n = num }).or_insert(num),
+                        GREEN_STR => game_cube_colors.entry(GREEN_STR).and_modify(|n| if *n < num { *n = num }).or_insert(num),
+                        BLUE_STR => game_cube_colors.entry(BLUE_STR).and_modify(|n| if *n < num { *n = num }).or_insert(num),
+                        _ => panic!("Invalid color")
+                    };
+                } else {
+                    match color.unwrap() {
+                        RED_STR => cube_colors.entry(RED_STR).and_modify(|n| *n += num).or_insert(num),
+                        GREEN_STR => cube_colors.entry(GREEN_STR).and_modify(|n| *n += num).or_insert(num),
+                        BLUE_STR => cube_colors.entry(BLUE_STR).and_modify(|n| *n += num).or_insert(num),
+                        _ => panic!("Invalid color")
+                    };
+                }
             }
-            if *cube_colors.get(RED_STR).unwrap_or(&0) > RED_CUBES
+
+            if !minimum_values
+                && (*cube_colors.get(RED_STR).unwrap_or(&0) > RED_CUBES
                 || *cube_colors.get(GREEN_STR).unwrap_or(&0) > GREEN_CUBES
-                || *cube_colors.get(BLUE_STR).unwrap_or(&0) > BLUE_CUBES {
+                || *cube_colors.get(BLUE_STR).unwrap_or(&0) > BLUE_CUBES) {
                 println!("{}: {}", game_name, round);
                 return 0;
             }
+        }
+
+        // Part 2 return
+        if minimum_values {
+            println!("{}: {:?}", game_name, game_cube_colors);
+            return game_cube_colors.values().product::<u32>();
         }
 
         return game_name.split(" ").nth(1).unwrap().parse::<u32>().unwrap();
